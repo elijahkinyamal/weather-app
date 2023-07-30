1b5359fc396de9ef646b3303608f8c0a1301787d
ELIJAH Kinyamal <94557540+elijahkinyamal@users.noreply.github.com>
2023-07-31 00:30:11 +0300

    Update App.js

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -10,11 +10,11 @@ function App() {
       </div>
       <div className="footer-info">
         <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
-          Source Code
+          code
         </a>{" "}
         | Developed by{" "}
         <a target="_blank" href="https://www.gauravghai.dev/">
-          Gaurav
+          Elijah
         </a>{" "}
         | Powered by{" "}
         <a target="_blank" href="https://www.htmlhints.com/">
